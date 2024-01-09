d80967abcf3b6a9a416945b7904952cdc383dd8e
Vojan Najov <serejah.mashichev@gmail.com>
2024-01-10 01:19:58 +0300

    Add point marshall dump/load sample

---
+++ the_ruby_programming_language/point/point_marshal.rb
@@ -0,0 +1,27 @@
+class Point
+
+  def initialize(*coords)
+    @coords = coords
+  end
+
+  def marshal_dump
+    @coords.pack("w*")
+  end
+
+  def marshal_load(s)
+    @coords = s.unpack("w*")
+  end
+
+  def to_s
+    @coords.to_s
+  end
+
+end
+
+p1 = Point.new(1, 2, 3, 4)
+puts p1
+s = p1.marshal_dump
+p2 = Point.new(0, 0, 0)
+p2.marshal_load(s)
+puts p2
+
